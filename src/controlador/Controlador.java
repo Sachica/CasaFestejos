b25405e6d1554d90aee7adb3419ba71c6d1c4e44
kuroy <kuroy@Sachi>
2019-05-05 11:29:10 -0500

    Prueba ramas master

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -14,7 +14,7 @@ import modelo.*;
 public class Controlador {
     public static void main(String[] args) {
         try {
-            System.out.println("");
+            System.out.println("Hola");
             Evento e = new Evento();
             e.agregarActividad(new Actividad("Karaoke", new Hora(1, 1, 1), 2000));
             e.agregarActividad(new Actividad("Karaoke", new Hora(1, 1, 1), 2000));
