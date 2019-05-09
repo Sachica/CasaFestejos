b35b6ff2510c06636acd2d9e8ffca0645379c805
AngeloSalamanca <angelo_roger@hotmail.com>
2019-05-09 11:15:23 -0500

    Complementación Del Crud De ResposableServicios

--- src/servicio/ResponsableServicio.java
+++ src/servicio/ResponsableServicio.java
@@ -35,4 +35,49 @@ public class ResponsableServicio {
         }
     }
     
+    public void Actualizar(Connection conexion, Responsable persona) throws SQLException{
+        
+        try{
+            PreparedStatement consulta;
+            consulta = conexion.prepareStatement("UPDATE persona SET nombre=?, apellidos=?, cedula=?, telefono=?, e_mail=?"
+                    +"WHERE cedula=?" );
+            
+            consulta.setString(1, persona.getNombre());
+            consulta.setString(2, persona.getApellidos());
+            consulta.setString(3, persona.getCedula());
+            consulta.setString(4, persona.getTelefono());
+            consulta.setString(5, persona.getE_mail());
+            consulta.executeUpdate();
+            
+        }catch(SQLException ex){
+            throw new SQLException(ex);
+        }
+    }
+    
+    public static void eliminar(Connection conexion, String cedula) throws SQLException{
+      try{
+          
+         PreparedStatement consulta;
+         consulta = conexion.prepareStatement("DELETE FROM persona "
+                 + "WHERE cedula=?");        
+         consulta.setString(3, cedula);
+         consulta.executeUpdate();
+      }catch(SQLException ex){
+         throw new SQLException(ex);
+      }
+   }
+    
+    public static void buscar(Connection conexion, String cedula) throws SQLException{
+      try{
+          
+         PreparedStatement consulta;
+         consulta = conexion.prepareStatement("SELECT * FROM persona "
+                 + "WHERE cedula=?");        
+         consulta.setString(3, cedula);
+         consulta.executeUpdate();
+        }catch(SQLException ex){
+         throw new SQLException(ex);
+        }
+    
+    }
 }
